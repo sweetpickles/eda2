b491cb400b3cf5aad1d187c7318703d195069a25
sweetpickles <yednap@hotmail.com>
2015-10-23 17:01:45 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,6 +4,6 @@ SCC <- readRDS("Source_Classification_Code.rds")
 p1 <- tapply(NEI$Emissions,NEI$year,sum)
 p1a <- as.data.frame(as.matrix(p1))
 
-plot(rownames(p1a),p1a$V1)
+plot(rownames(p1a),p1a$V1,main="PM2.5 Emissions vs Year for United States",xlab="year",ylab="Total Emissions (tons PM2.5)")
 model <- lm(p1a$V1 ~ rownames(p1a))
 abline(model)

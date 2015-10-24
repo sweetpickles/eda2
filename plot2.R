34d1ab762623619577d17124c392f09dca7a58ae
sweetpickles <yednap@hotmail.com>
2015-10-23 17:02:45 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,6 +5,6 @@ ds2 <- NEI[NEI$fips == '24510',]
 p2 <- tapply(ds2$Emissions,ds2$year,sum)
 p2a <- as.data.frame(as.matrix(p2))
 
-plot(rownames(p2a),p2a$V1)
+plot(rownames(p2a),p2a$V1,main="PM2.5 Emissions vs Year for Baltimore City",xlab="year",ylab="Total Emissions (tons PM2.5)")
 model <- lm(p2a$V1 ~ as.numeric(rownames(p2a)))
 abline(model)
